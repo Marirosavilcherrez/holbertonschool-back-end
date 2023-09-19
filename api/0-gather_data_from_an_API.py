a66a97aeed7052e3593214d7b3af59520900ed20
Marirosa <arqmvilcherrez@gmail.com>
2023-09-19 14:18:25 -0700

    Fix name variable error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,28 +6,29 @@ import sys
 def give_employee_todo(employee_id):
     "URL API to consult"
     api_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    api_url2 = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
 
     "make a GET request to the API"
     response = requests.get(api_url)
+    response2 = requests.get(api_url2)
 
     "check if the request was success"
-    if response.status_code == 200:
+    if response.status_code == 200 and response2.status_code == 200:
         todos = response.json()
+        user_data = response2.json()
 
         "Count the number of completed and total tasks"
-        EMPLOYEE_NAME = 'name'
-        TASK_TITLE = 'title'
+        EMPLOYEE_NAME = user_data['name']
         TOTAL_NUMBER_OF_TASKS = len(todos)
         NUMBER_OF_DONE_TASKS = sum(1 for todo in todos if todo['completed'])
 
         "print employee TODO list"
-        print('Employee {} is done with tasks({}/{}):'\
-              .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+        print(f'Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
 
         "Print the titles of the completed task"
         for todo in todos:
             if todo['completed']:
-                print('\t{}'.format(todo[TASK_TITLE]))
+                print(f'\t{todo["title"]}')
     else:
         print(f'Error: Unable to fetch data for employee {employee_id}')
 

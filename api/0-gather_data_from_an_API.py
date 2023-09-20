ab748a3267c697c018baef3d73cfeac54e53d711
Marirosa <arqmvilcherrez@gmail.com>
2023-09-19 21:17:43 -0700

    Add funcions to the code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,48 @@
 #!/usr/bin/python3
 """Python script that using REST API"""
-from requests import get
-from sys import argv
+import requests
+import sys
 
+def fetch_user_data(user_id):
+    try:
+        url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        user_data = response.json()
+        return user_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error al obtener datos del usuario: {e}")
+        sys.exit(1)
+
+def fetch_user_todos(user_id):
+    try:
+        url = "https://jsonplaceholder.typicode.com/todos"
+        params = {"userId": user_id}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+
+        todos = response.json()
+        return todos
+    except requests.exceptions.RequestException as e:
+        print(f"Error al obtener las tareas del usuario: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    todos = response.json()
-    number_tasks = 0
-    total_tasks = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    user_data = response2.json()
+    if len(sys.argv) != 2:
+        print("Uso: python script.py <user_id>")
+        sys.exit(1)
 
-    for user in user_data:
-        if user.get("id") == int(argv[1]):
-            employee_id = user.get("name")
+    user_id = sys.argv[1]
+
+    user_data = fetch_user_data(user_id)
+    todos = fetch_user_todos(user_id)
+
+    total_tasks = len(todos)
+    completed_tasks = sum(1 for todo in todos if todo["completed"])
+
+    print(f"Empleado {user_data.get('name')} ha completado tareas ({completed_tasks}/{total_tasks}):")
 
     for todo in todos:
-        if todo.get("userId") == int(argv[1]):
-            total_tasks += 1
-
-            if todo.get('completed') is True:
-                number_tasks += 1
-                tasks.append(todo.get("title"))
-
-    print(f"Employee {employee_id} is done with tasks("
-          f"{number_tasks}/{total_tasks}):")
-
-    for task in tasks:
-        print("\t {}".format(task))
+        if todo["completed"]:
+            print(f"\t {todo['title']}")

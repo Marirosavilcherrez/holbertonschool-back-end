4b25e6edec9b6daef378dfed7b546506c2e3b7ed
Marirosa <arqmvilcherrez@gmail.com>
2023-09-19 18:48:47 -0700

    Fix employee_name error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,26 +7,26 @@ from sys import argv
 if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
     todos = response.json()
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
+    number_tasks = 0
+    total_tasks = 0
     tasks = []
     response2 = get('https://jsonplaceholder.typicode.com/users')
     user_data = response2.json()
 
     for user in user_data:
-        if user.get('id') == int(argv[1]):
-            employee_id = user.get('name')
+        if user.get("id") == int(argv[1]):
+            employee_id = user.get("name")
 
     for todo in todos:
-        if todo.get('userId') == int(argv[1]):
-            TOTAL_NUMBER_OF_TASKS += 1
+        if todo.get("userId") == int(argv[1]):
+            total_tasks += 1
 
             if todo.get('completed') is True:
-                NUMBER_OF_DONE_TASKS += 1
-                tasks.append(todo.get('title'))
+                number_tasks += 1
+                tasks.append(todo.get("title"))
 
     print(f"Employee {employee_id} is done with tasks("
-          f"{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+          f"{number_tasks}/{total_tasks}):")
 
     for task in tasks:
         print("\t {}".format(task))

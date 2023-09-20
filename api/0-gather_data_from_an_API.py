7bbc2f9a92538fc4c48b4ae3d5817cdf04f6d5cf
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-09-19 23:23:22 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,46 +3,26 @@
 import requests
 import sys
 
-def fetch_user_data(user_id):
-    try:
-        url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-        response = requests.get(url)
-        response.raise_for_status()
-
-        user_data = response.json()
-        return user_data
-    except requests.exceptions.RequestException as e:
-        print(f"Error al obtener datos del usuario: {e}")
-        sys.exit(1)
-
-def fetch_user_todos(user_id):
-    try:
-        url = "https://jsonplaceholder.typicode.com/todos"
-        params = {"userId": user_id}
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-
-        todos = response.json()
-        return todos
-    except requests.exceptions.RequestException as e:
-        print(f"Error al obtener las tareas del usuario: {e}")
-        sys.exit(1)
+import requests
+import sys
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+
     if len(sys.argv) != 2:
         print("Uso: python script.py <user_id>")
         sys.exit(1)
 
     user_id = sys.argv[1]
 
-    user_data = fetch_user_data(user_id)
-    todos = fetch_user_todos(user_id)
+    user = requests.get(url + "/users/{}".format(user_id)).json()
+    todos = requests.get(url + "/todos", params={"userId": user_id}).json()
 
     total_tasks = len(todos)
     completed_tasks = sum(1 for todo in todos if todo["completed"])
 
-    print(f"Empleado {user_data.get('name')} ha completado tareas ({completed_tasks}/{total_tasks}):")
+    print("First line: Employee {} is done with tasks ({}/{}):".format(user.get("name"), completed_tasks, total_tasks))
 
     for todo in todos:
         if todo["completed"]:
-            print(f"\t {todo['title']}")
+            print("\t{}".format(todo["title"]))
